6e5c870667714ab77578dd7ca8947d318188e0d1
Bshields87 <shields.bethany@gmail.com>
2019-04-03 14:55:18 -0700

    styled

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 // htmlRoutes(app)
 
 app.use(express.urlencoded({extended: true}))
+app.use(express.static("./app/public"));
 
 const htmlRoutes = require('./app/routing/htmlRoutes')
 const apiRoutes = require('./app/routing/apiRoutes')
